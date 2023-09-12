499c63facf8f6972d3b52ba18db960821dec4b26
Augusto Castillo <a.castillo.vergara@gmail.com>
2023-09-12 19:49:55 -0300

    try que realiza una solicitud a la api web con fetch
    Tambien se convierte la respuesta en formato

--- static/app.js
+++ static/app.js
@@ -1,4 +1,12 @@
 async function buscarUsuario() {
   const campo = document.getElementById("campo").value;
   const tipoBusqueda = document.getElementById("tipoBusqueda").value;
+  try {
+    const response = await fetch(`/buscar/${tipoBusqueda}/${campo}`);
+    const data = await response.json();
+
+    mostrarResultados(data);
+  } catch (error) {
+    console.error("Error al buscar usuario:", error);
+  }
 }

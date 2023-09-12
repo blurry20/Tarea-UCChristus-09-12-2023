5487787e47379e9c9de170b694a3d9796c16238d
Augusto Castillo <a.castillo.vergara@gmail.com>
2023-09-12 19:40:29 -0300

    Se establece la conexión con la base de datos SQLite.

--- scripts/api.py
+++ scripts/api.py
@@ -1,5 +1,7 @@
 import os
+import sqlite3
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse, FileResponse
 from starlette.staticfiles import StaticFiles
 from starlette.middleware.cors import CORSMiddleware
 from fastapi.middleware import Middleware
@@ -16,3 +18,17 @@ middleware = [
 ]
 
 app.mount("/static", StaticFiles(directory=static_dir), name="static")
+
+
+@app.get("/")
+async def read_index():
+    return FileResponse(os.path.join(static_dir, "index.html"))
+
+
+def conexion_db():
+    try:
+        conn = sqlite3.connect("MOCK_DATA.db")
+        return conn
+    except sqlite3.Error as error:
+        print("Error al conectar con la base de datos", error)
+        return None

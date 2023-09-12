8decc3d439dc194a81c084acf35d69ee93efdeb1
Augusto Castillo <a.castillo.vergara@gmail.com>
2023-09-12 19:36:49 -0300

    se define el directorio para los archivos estaticos

--- scripts/api.py
+++ scripts/api.py
@@ -1,3 +1,15 @@
+import os
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from fastapi.middleware import Middleware
+
 
 app = FastAPI()
+
+static_dir = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), "../static")
+
+middleware = [
+    Middleware(CORSMiddleware, allow_origins=[
+               "*"], allow_methods=["*"], allow_headers=["*"])
+]
